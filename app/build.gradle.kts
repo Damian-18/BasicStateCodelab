4fc8aefe75d33e5dba7a875c75f727453443d51c
Damian-18 <s23120229@alumnos.itsur.edu.mx>
2026-09-23 21:01:44 -0600

    refactor: migrar estado y lógica de la lista a WellnessViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
+
 }
